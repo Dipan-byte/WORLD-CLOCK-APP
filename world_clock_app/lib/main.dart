9c268f3b89ea10707308d3130f87a947e1f03a0f
Dipan Mallick <dipanmallick39@gmail.com>
2025-11-27 23:46:08 +0530

    routes are set

--- world_clock_app/lib/main.dart
+++ world_clock_app/lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:world_clock_app/screens/home.dart';
+import 'package:world_clock_app/screens/loading_screen.dart';
+import 'package:world_clock_app/screens/choose_location.dart';
+
 
 
 void main() {
   runApp(MaterialApp(
-    home:Home() 
+    initialRoute: '/home',
+    routes: {
+      '/' : (context) => LoadingScreen() ,
+      '/home' : (context) => Home() ,
+      '/location': (context) => ChooseLocation() 
+    },
   ));
 }
 
---
+++ world_clock_app/lib/screens/choose_location.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Center(
+        child: Text('location screen')
+      )
+
+    );;
+  }
+}
