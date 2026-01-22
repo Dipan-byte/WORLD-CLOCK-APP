a28c1014e41402eebdcd37f57ab858ed2df4381b
Dipan Mallick <dipanmallick39@gmail.com>
2026-01-23 01:26:42 +0530

    creating the api request to get the json data of the time

--- world_clock_app/lib/main.dart
+++ world_clock_app/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:world_clock_app/screens/choose_location.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/home',
+  
     routes: {
       '/' : (context) => LoadingScreen() ,
       '/home' : (context) => Home() ,
--- world_clock_app/lib/screens/loading_screen.dart
+++ world_clock_app/lib/screens/loading_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
 
 class LoadingScreen extends StatefulWidget {
-  
-
   @override
   State<LoadingScreen> createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getTime()async{
+    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Kolkata')) ;
+
+    Map data = jsonDecode(response.body);
+    print(data) ;
+  }
+  @override
+  void initState() {
+    super.initState();
+    getTime() ;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
