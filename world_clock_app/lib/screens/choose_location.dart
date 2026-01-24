a96bbf95d66f13dfb03b7e6e982610aa140bf153
Dipan Mallick <dipanmallick39@gmail.com>
2026-01-24 22:08:11 +0530

    location page makiing done use listbuilder , listtile

--- world_clock_app/lib/screens/choose_location.dart
+++ world_clock_app/lib/screens/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_clock_app/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
 
@@ -7,6 +8,16 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<World_time> locations = [
+    World_time(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    World_time(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    World_time(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    World_time(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    World_time(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    World_time(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    World_time(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    World_time(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,10 +26,27 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title:  Text('CHOOSE LOCATION') ,
 
       ),
-       body: Center(
-        child: Text('location screen')
-      )
+       body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context , index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0 , horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: (){
+                  
+                } ,
+                  title: Text(locations[index].location),
+                  leading : CircleAvatar(
+                    backgroundImage: AssetImage('assets/${locations[index].flag}')
+                    ),
+                
+              ),
+            ),
+          );
+        }
+       )
 
-    );;
+    );
   }
 }
